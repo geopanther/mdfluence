7d4510bd64574741c9b9d20b1e084d6329aeb260
geopanther <geopanther@mail.de>
2026-07-27 21:51:10 +0200

    feat: export stable library surface from mdfluence package
    Re-export MinimalConfluence, Page, get_pages_from_directory, publish,
    PublishOptions, Reporter, NullReporter, RelativeLinkError and
    apply_title_prefix so downstream code can embed mdfluence as a library.

--- mdfluence/__init__.py
+++ mdfluence/__init__.py
@@ -1,2 +1,25 @@
 __version__ = "0.4.1"
 __url__ = "https://github.com/geopanther/mdfluence"
+
+from mdfluence.api import MinimalConfluence
+from mdfluence.document import Page, get_pages_from_directory
+from mdfluence.sync import (
+    NullReporter,
+    PublishOptions,
+    RelativeLinkError,
+    Reporter,
+    apply_title_prefix,
+    publish,
+)
+
+__all__ = [
+    "MinimalConfluence",
+    "NullReporter",
+    "Page",
+    "PublishOptions",
+    "RelativeLinkError",
+    "Reporter",
+    "apply_title_prefix",
+    "get_pages_from_directory",
+    "publish",
+]
